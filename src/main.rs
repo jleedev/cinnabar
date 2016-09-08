cc76c491e61c9732faf04bea84d77461f17fce7e
Josh Lee <jleedev@gmail.com>
2016-09-08 14:00:57 +0000

    it verifies some hashes!  others break it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate mmap;
 extern crate rustc_serialize;
+extern crate crypto;
 
 // TODO: Either generalize this code to Seek+Read, or extend MemoryMap with
 // those traits.
@@ -12,40 +13,80 @@ mod revlog;
 
 use std::{error, result};
 use rustc_serialize::hex::ToHex;
+use crypto::sha1::Sha1;
+use crypto::digest::Digest;
 
 fn read_revlog(path: &str) -> result::Result<(), Box<error::Error>> {
     let revlog = try!(revlog::Revlog::open(path));
 
+    /*
     println!("   rev    offset  length  {} linkrev nodeid       p1           p2",
              if revlog.generaldelta {
                  "delta"
              } else {
                  " base"
              });
+    */
 
+    let mut i = 0;
     for entry in revlog.iter() {
         let entry = try!(entry);
 
-        let p1 = entry.parent_1_id().map(|s| s.to_hex()).unwrap();
-        let p2 = entry.parent_2_id().map(|s| s.to_hex()).unwrap();
+        let p1 = entry.parent_1_id().unwrap();
+        let p2 = entry.parent_2_id().unwrap();
+        let node_id = entry.chunk.c_node_id().to_hex();
+        /*
         println!("{:6} {:9} {:7} {:6} {:7} {} {} {}",
                  entry.revno,
                  entry.offset(),
                  entry.chunk.comp_len(),
                  entry.base_rev(),
                  entry.chunk.link_rev(),
-                 &entry.chunk.c_node_id()[..6].to_hex(),
-                 &p1[..12],
-                 &p2[..12]);
+                 &node_id[..12],
+                 &p1.to_hex()[..12],
+                 &p2.to_hex()[..12]);
+        */
 
         let mut chain: Vec<_>;
         chain = entry.delta_chain().map(Result::unwrap).collect();
-        chain.reverse();
         let base = chain.pop().unwrap();
+        chain.reverse();
         let patches: Vec<Vec<u8>>;
         patches = chain.iter().map(|rev| rev.data()).collect();
-        patch::apply(base.data(), patches);
+        let text = patch::apply(base.data(), patches);
+        //println!("{:?}", String::from_utf8_lossy(&text));
+
+        let mut sha = Sha1::new();
+        let mut ps = vec![p1, p2];
+        ps.sort();
+        for p in &ps {
+            sha.input(p);
+        }
+        sha.input(&text);
+        let hex = sha.result_str();
+        /*
+        if node_id != hex {
+            println!("ERROR");
+            println!("{:6} {:9} {:7} {:6} {:7} {} {} {}",
+                     entry.revno,
+                     entry.offset(),
+                     entry.chunk.comp_len(),
+                     entry.base_rev(),
+                     entry.chunk.link_rev(),
+                     &node_id[..12],
+                     &p1.to_hex()[..12],
+                     &p2.to_hex()[..12]);
+            for p in &ps {
+                println!("{:?}", p.to_hex());
+            }
+            println!("{:?}", String::from_utf8_lossy(&text));
+        }
+        */
+        assert_eq!(node_id, hex);
+        println!("{:?}", hex);
+        i = i + 1;
     }
+    println!("{} hashes verified", i);
     Ok(())
 }
 
--- src/patch.rs
+++ src/patch.rs
@@ -5,38 +5,54 @@
 //! Each patch is a sequence of length-delimited hunks.
 //! Each hunk contains three u32le values followed by `c` data bytes.
 //!
-//! `a` - seek to this position in the buffer  
-//! `b` - delete to this position in the buffer (`b` &ge; `a`)  
+//! `a` - seek to this position in the buffer
+//! `b` - delete to this position in the buffer (`b` &ge; `a`)
 //! `c` - length of the value to insert
 //!
-//! This implementation is as naive as possible: it actually copies and
-//! moves all the bytes around as needed. Areas for improvement: Use a
-//! data structure that can perform this lazily.
+//! This implementation uses an off-the-shelf rope structure to perform
+//! this editing.
 
 extern crate byteorder;
+extern crate bytes;
 
-use patch::byteorder::{BigEndian,ReadBytesExt};
-use std::io::{Cursor,Read};
+use patch::byteorder::{BigEndian, ReadBytesExt};
+use std::io::{Cursor, Read};
+use self::bytes::{Bytes, Source};
 
 pub fn apply(base: Vec<u8>, patches: Vec<Vec<u8>>) -> Vec<u8> {
-    let mut buf = Cursor::new(base);
+    //println!("::: {:?}", String::from_utf8_lossy(&base));
+    let mut buf: Bytes = From::from(&base);
     for patch in patches {
-        let len = patch.len() as u64;
+        let patch_len = patch.len() as u64;
         let mut cur = Cursor::new(patch);
-        while cur.position() != len {
+        while cur.position() != patch_len {
+            assert!(cur.position() < patch_len);
+            let before_len = buf.len();
             let (a, b, c) = decode_header(&mut cur);
-            println!("{} {} {}", a, b, c);
-            let mut x = Vec::with_capacity(c as usize);
-            cur.read_exact(&mut x[..]).unwrap();
+            let piece = read_slice(&mut cur, c);
+            //println!("+++ {} {} {} {:?}", a, b, c, String::from_utf8_lossy(&piece));
+            let insertion: Bytes = From::from(&piece);
+            let left = buf.slice_to(a);
+            let right = buf.slice_from(b);
+            buf = left.concat(&insertion).concat(&right);
+            assert_eq!(before_len - (b - a) + c, buf.len());
         }
-    };
-    return vec![];
+    }
+    let mut result = vec![];
+    buf.copy_to(&mut result);
+    return result;
 }
 
-fn decode_header(header: &mut Cursor<Vec<u8>>) -> (u32, u32, u32) {
-    let a = header.read_u32::<BigEndian>().unwrap();
-    let b = header.read_u32::<BigEndian>().unwrap();
-    let c = header.read_u32::<BigEndian>().unwrap();
+fn read_slice(src: &mut Cursor<Vec<u8>>, len: usize) -> Vec<u8> {
+    let mut buf = vec![0; len];
+    src.read_exact(&mut buf[..]).unwrap();
+    return buf;
+}
+
+fn decode_header(header: &mut Cursor<Vec<u8>>) -> (usize, usize, usize) {
+    let a = header.read_u32::<BigEndian>().unwrap() as usize;
+    let b = header.read_u32::<BigEndian>().unwrap() as usize;
+    let c = header.read_u32::<BigEndian>().unwrap() as usize;
     return (a, b, c);
 }
 
@@ -46,8 +62,7 @@ mod test {
     use std::io::Cursor;
     #[test]
     fn test_header() {
-        let mut hdr = Cursor::new(
-            b"\x00\x00\x00\x2a\x00\x00\x00\x2b\x00\x00\x00\x2c" as &[u8]);
+        let mut hdr = Cursor::new(b"\x00\x00\x00\x2a\x00\x00\x00\x2b\x00\x00\x00\x2c" as &[u8]);
         assert_eq!((0x2a, 0x2b, 0x2c), decode_header(&mut hdr));
     }
 }
